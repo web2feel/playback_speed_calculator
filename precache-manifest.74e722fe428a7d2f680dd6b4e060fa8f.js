ed825be0cfbf5c15187d8574af658381a8f726c6
Jinson Abraham <web2feel@gmail.com>
2020-08-01 08:24:38 +0530

    Updates

---
+++ precache-manifest.74e722fe428a7d2f680dd6b4e060fa8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0f0b0a45917561c9e8393aec0bc8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a479ed77ae94e4ccfac",
+    "url": "/static/css/2.bf0d58e5.chunk.css"
+  },
+  {
+    "revision": "02f0be7f175d361d0db6",
+    "url": "/static/css/main.a288fbe1.chunk.css"
+  },
+  {
+    "revision": "8a479ed77ae94e4ccfac",
+    "url": "/static/js/2.f1f3f898.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f1f3f898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f0be7f175d361d0db6",
+    "url": "/static/js/main.b3a2cb4d.chunk.js"
+  },
+  {
+    "revision": "c95a4889da3821ff8ee2",
+    "url": "/static/js/runtime-main.a6718590.js"
+  }
+]);
